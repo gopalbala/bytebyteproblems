e76a5a29edad2c751d0a6ff1dfd4b556dfe6f1ff
Gopalakrishnan <fax6@INMACY4AAH.local>
2017-10-23 10:28:45 +0530

    1. Added two problems check if the array can be divided in to pairs in such a way the sum of them is divisible by the number. 2. value of the node is the sum of all the nodes whose value is greater than the value of the node

---
+++ src/main/java/array/ArrayDivisiblePair.java
@@ -0,0 +1,50 @@
+package array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by fax6 on 23/10/17.
+ */
+public class ArrayDivisiblePair {
+    public static void main(String[] args) {
+        int arr[] = {92, 75, 65, 48, 45, 35};
+        int k = 10;
+        System.out.println(checkPairs(arr, k));
+    }
+
+    static boolean checkPairs(int[] arr, int k) {
+        if (arr.length % 2 == 1)
+            return false;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int remainder = arr[i] % k;
+            if (!hashMap.containsKey(remainder)) {
+                hashMap.put(remainder, 1);
+            } else {
+                hashMap.put(remainder, hashMap.get(remainder) + 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> map : hashMap.entrySet()) {
+            System.out.println(map.getKey() + ":" + map.getValue());
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int remainder = arr[i] % k;
+            if (remainder * 2 == k) {
+                //check in the has map for even occurances of such remainder
+                if (hashMap.get(remainder) % 2 == 1) {
+                    return false;
+                } else if (remainder == 0) {
+                    if (hashMap.get(remainder) % 2 == 1) {
+                        return false;
+                    }
+                } else {
+                    if (hashMap.get(k - remainder) != hashMap.get(remainder))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/trees/AddGreaterNodes.java
@@ -0,0 +1,86 @@
+package trees;
+
+/**
+ * Created by fax6 on 23/10/17.
+ */
+public class AddGreaterNodes {
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+
+          /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80 */
+
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        tree.modifySum(tree.root, new Sum());
+
+        tree.inorder();
+
+    }
+}
+
+class BinarySearchTree {
+    Node root;
+
+    void inorder() {
+        inorderTraversal(root);
+    }
+
+    public void insert(int data) {
+        root = insertRec(root, data);
+    }
+
+    void inorderTraversal(Node node) {
+        if (node == null)
+            return;
+        inorderTraversal(node.left);
+        System.out.print(node.data + " ");
+        inorderTraversal(node.right);
+    }
+
+    Node insertRec(Node node, int data) {
+        if (node == null) {
+            root = new Node(data);
+            return root;
+        }
+        if (data < node.data) {
+            node.left = insertRec(node.left, data);
+        } else {
+            node.right = insertRec(node.right, data);
+        }
+        return node;
+    }
+
+    void modifySum(Node node, Sum sum) {
+        if (node == null)
+            return;
+        modifySum(node.right, sum);
+        sum.sum += node.data;
+        node.data = sum.sum;
+        modifySum(node.left, sum);
+    }
+}
+
+class Sum {
+    int sum;
+}
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
